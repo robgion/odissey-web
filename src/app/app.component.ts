a348b237376cc55dac633a599d230ea5ca457ef2
robgion <r.giontella@2clever.it>
2021-05-05 17:59:52 +0200

    I/O e Content projection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,38 @@ export class AppComponent {
 
   cardTitle1 = 'Card title 1';
   cardTitle2 = 'Card title 2';
+
+  cards: any[];
+
+
+  constructor() {
+    this.cards = [
+      {
+        itemIdx: 0,
+        showBody: true,
+        title: 'Card title 1'
+      },
+      {
+        itemIdx: 1,
+        showBody: false,
+        title: 'Card title 2'
+      },
+      {
+        itemIdx: 2,
+        showBody: false,
+        title: 'Card title 3'
+      }
+    ];
+  }
+
+  manageToggle(data: any): void{
+    console.log(data);
+    if (data.showBody) {
+      this.cards.forEach(
+          el => {
+              el.showBody = el.itemIdx === data.idx;
+          }
+      );
+    }
+  }
 }
--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,55 +1,67 @@
-import {Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges} from '@angular/core';
 
 @Component({
-  selector: 'tcs-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+    selector: 'tcs-card',
+    templateUrl: './card.component.html',
+    styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit, OnDestroy, OnChanges {
 
-  @Input() title: string;
-  showBody: boolean;
+    @Output() toggleEvent: EventEmitter<any> = new EventEmitter<any>();
+    @Input() itemIdx: number;
+    @Input() title: string;
+    @Input() showBody: boolean;
 
-  /**
-   * Lifecycle: 1 ( Una tantum )
-   */
-  constructor() {
-    this.title = 'Card title';
-    this.showBody = true;
-    console.log('CardComponent loading...');
-    console.log(this.title);
-  }
+    /**
+     * Lifecycle: 1 ( Una tantum )
+     */
+    constructor() {
+        this.itemIdx = 0;
+        this.title = 'Card title';
+        this.showBody = true;
+        console.log('CardComponent loading...');
+        console.log(this.title);
+    }
 
-  /**
-   * Lifecycle: 2
-   * Meotodo ( Lifecycle Hooks ) che viene chiamato:
-   * 1. dopo il costruttore
-   * 2. tutte le volte che recepisce un cambiamento del valore in input ( Lifecycle: n )
-   */
-  ngOnChanges(changes: SimpleChanges): void {
-    console.log('CardComponent ngOnChanges...');
-    console.log(this.title);
-  }
+    /**
+     * Lifecycle: 2
+     * Meotodo ( Lifecycle Hooks ) che viene chiamato:
+     * 1. dopo il costruttore
+     * 2. tutte le volte che recepisce un cambiamento del valore in input ( Lifecycle: n )
+     */
+    ngOnChanges(changes: SimpleChanges): void {
+        console.log('CardComponent ngOnChanges...');
+        console.log(this.title);
+    }
 
-  /**
-   * Lifecycle: 3 ( una tantum )
-   * Metodo dell'interfaccia OnInit ovvero uno dei Lifecycle Hooks.
-   * Viene eseguito dopo il costruttore ma prima del rendering della pagina.
-   * Qui piazziamo le inizializzazioni del componente con eventuali "side effect" come ad es. chiamate HTTP.
-   */
-  ngOnInit(): void {
-    console.log('CardComponent init...');
-    console.log(this.title);
-  }
+    /**
+     * Lifecycle: 3 ( una tantum )
+     * Metodo dell'interfaccia OnInit ovvero uno dei Lifecycle Hooks.
+     * Viene eseguito dopo il costruttore ma prima del rendering della pagina.
+     * Qui piazziamo le inizializzazioni del componente con eventuali "side effect" come ad es. chiamate HTTP.
+     */
+    ngOnInit(): void {
+        console.log('CardComponent init...');
+        console.log(this.title);
+    }
 
-  /**
-   * Lifecycle: n ( una tantum )
-   * Eseguito all'atto della distruzione del componente dal layout ( Lifecycle Hooks )
-   */
-  ngOnDestroy(): void {
-  }
+    /**
+     * Lifecycle: n ( una tantum )
+     * Eseguito all'atto della distruzione del componente dal layout ( Lifecycle Hooks )
+     */
+    ngOnDestroy(): void {
+    }
 
-  toggle(): void {
-    this.showBody = !this.showBody;
-  }
+    toggle(): void {
+        this.showBody = !this.showBody;
+
+        const value = {
+            idx: this.itemIdx,
+            showBody: this.showBody
+        };
+
+        this.toggleEvent.emit(
+            value
+        );
+    }
 }
